0c329e8ba728da272a5e0db613536c83ca75d20a
BasilAnna <chinnuannaabrham@gmail.com>
2022-08-22 12:41:14 -0400

    intial setup

---
+++ src/test/java/com/qaguru/shoptest/tests/ShopTest.java
@@ -0,0 +1,10 @@
+package com.qaguru.shoptest.tests;
+
+import org.testng.annotations.Test;
+
+public class ShopTest {
+    @Test
+    public void sampleTest(){
+        System.out.println("My Test");
+    }
+}
